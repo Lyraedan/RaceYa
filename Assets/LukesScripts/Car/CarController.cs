6b64415206d4ee9271cfe92705763e15e3556746
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-02-06 18:31:56 +0000

    Super basic car controller and hitbox visualiser

---
+++ Assets/LukesScripts/Car/CarController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Controls")]
+    public KeyCode forward = KeyCode.W;
+    public KeyCode back = KeyCode.S;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    [Header("Car properties")]
+    public float maxSpeedForward = 60f;
+    public float maxSpeedBack = 10f;
+    [Space(3)]
+    public float currentSpeed = 0f;
+    [Space(5)]
+    public float rateOfAcceleration = 3f;
+    public float rateOfReverseAcceleration = 1f;
+    [Header("Car Components")]
+    public WheelRotator wheelController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMovingForward)
+        {
+            wheelController.rotating = true;
+            wheelController.rotationSpeed = currentSpeed * 10;
+            currentSpeed += rateOfAcceleration;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeedForward);
+            transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed, Space.Self);
+        } else if(isMovingBack)
+        {
+            wheelController.rotating = true;
+            wheelController.rotationSpeed = currentSpeed * 10;
+            currentSpeed -= rateOfReverseAcceleration;
+            currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeedBack, 0);
+            transform.Translate(-Vector3.forward * Time.deltaTime * currentSpeed, Space.Self);
+        } else
+        {
+            if (currentSpeed > 0)
+            {
+                currentSpeed--;
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeedForward);
+            } else
+            {
+                currentSpeed++;
+                currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeedBack, 0);
+            }
+            wheelController.rotating = false;
+        }
+    }
+
+    public bool isMovingForward
+    {
+        get { return Input.GetKey(forward); }
+    }
+
+    public bool isMovingBack
+    {
+        get { return Input.GetKey(forward); }
+    }
+
+    public bool isTurningLeft
+    {
+        get { return Input.GetKey(left); }
+    }
+
+    public bool isTurningRight
+    {
+        get { return Input.GetKey(right); }
+    }
+}
---
+++ Assets/LukesScripts/Car/HitboxVisualiser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxVisualiser : MonoBehaviour
+{
+
+    public bool displayHitboxes = true;
+    [SerializeField] private List<Hitbox> hitboxes = new List<Hitbox>();
+
+    private void OnDrawGizmos()
+    {
+        if (displayHitboxes)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position + transform.forward, 0.1f);
+            foreach (Hitbox hitbox in hitboxes)
+            {
+                if (hitbox.collider != null)
+                {
+                    Gizmos.matrix = transform.localToWorldMatrix;
+                    Gizmos.color = hitbox.isValid ? hitbox.visualColor : Color.red;
+                    if (hitbox.wireframe) Gizmos.DrawWireCube(hitbox.collider.center, hitbox.collider.size);
+                    else Gizmos.DrawCube(hitbox.collider.center, hitbox.collider.size);
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+class Hitbox
+{
+    public BoxCollider collider;
+    public Color visualColor = Color.green;
+    public bool wireframe = false;
+    public bool isValid { get { return collider.size.x > 0 && collider.size.y > 0 && collider.size.z > 0; } }
+}

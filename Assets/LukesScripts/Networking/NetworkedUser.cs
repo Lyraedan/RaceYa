c552573a5206d4c21c6044982b461739ae318dfe
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-02-12 09:47:13 +0000

    Setup network manager

---
+++ Assets/LukesScripts/Networking/NetworkedUser.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedUser : MonoBehaviour
+{
+
+    public GameObject camera;
+
+    private PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+        if(!view.IsMine)
+        {
+            Destroy(camera);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

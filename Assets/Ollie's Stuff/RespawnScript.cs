77dfff5ef57487f7e6329e0695f68a1d5da3f766
Ollie-Evenden <78206771+Ollie-Evenden@users.noreply.github.com>
2021-03-02 18:04:56 +0000

    Ollies scripts

---
+++ Assets/Ollie's Stuff/RespawnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    [SerializeField]  private Transform respawnPoint;
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("OtherPlayer"))
+        {
+            Debug.Log(collision);
+            collision.gameObject.transform.position = respawnPoint.transform.position;
+            if(collision.attachedRigidbody)
+            {
+                collision.attachedRigidbody.useGravity = false;
+                collision.attachedRigidbody.isKinematic = true;
+                collision.attachedRigidbody.useGravity = true;
+                collision.attachedRigidbody.isKinematic = false;
+            }
+        }
+    }
+}

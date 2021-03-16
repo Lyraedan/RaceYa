5430f62b8312494e5c70675eb19d35dd479dcefe
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-03-16 20:19:02 +0000

    Updated main menu with other buttons and setup animated hover

---
+++ Assets/LukesScripts/Scripts/Rock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public void BeginRock() => anim.SetBool("Rocking", true);
+
+    public void EndRock() => anim.SetBool("Rocking", false);
+}

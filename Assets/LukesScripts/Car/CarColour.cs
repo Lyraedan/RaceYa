56ee45b878e0be911966301ad239443f95aeac61
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-03-18 23:59:31 +0000

    Combined the menus together, made it so your car customizations are saved between sessions (colour picker doesn't load the loaded data correctly though so it'll still be red, red)

--- Assets/LukesScripts/Car/CarColour.cs
+++ Assets/LukesScripts/Car/CarColour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class CarColour : MonoBehaviour
@@ -10,6 +11,8 @@ public class CarColour : MonoBehaviour
     public Color bodyColour = Color.red;
     public Color characterColour = Color.red;
 
+    public UnityEvent OnStart;
+
     void Awake()
     {
         if (instance == null)
@@ -18,15 +21,48 @@ public class CarColour : MonoBehaviour
             Destroy(this);
     }
 
+    private void Start()
+    {
+        OnStart?.Invoke();
+    }
+
     public void SaveBody(GameObject preview)
     {
         if (gameObject.activeSelf)
+        {
             bodyColour = preview.GetComponent<Image>().material.GetColor("_Color1");
+            PlayerPrefs.SetFloat("CAR_BODY_RED", bodyColour.r);
+            PlayerPrefs.SetFloat("CAR_BODY_GREEN", bodyColour.g);
+            PlayerPrefs.SetFloat("CAR_BODY_BLUE", bodyColour.b);
+        }
     }
 
     public void SaveCharacter(GameObject preview)
     {
         if (gameObject.activeSelf)
+        {
             characterColour = preview.GetComponent<Image>().material.GetColor("_Color1");
+            PlayerPrefs.SetFloat("CAR_CHARACTER_RED", characterColour.r);
+            PlayerPrefs.SetFloat("CAR_CHARACTER_GREEN", characterColour.g);
+            PlayerPrefs.SetFloat("CAR_CHARACTER_BLUE", characterColour.b);
+        }
+    }
+
+    public void LoadBody(GameObject preview)
+    {
+        float bodyR = PlayerPrefs.GetFloat("CAR_BODY_RED");
+        float bodyG = PlayerPrefs.GetFloat("CAR_BODY_GREEN");
+        float bodyB = PlayerPrefs.GetFloat("CAR_BODY_BLUE");
+        bodyColour = new Color(bodyR, bodyG, bodyB);
+        preview.GetComponent<Image>().material.SetColor("_Color1", bodyColour);
+    }
+
+    public void LoadCharacter(GameObject preview)
+    {
+        float characterR = PlayerPrefs.GetFloat("CAR_CHARACTER_RED");
+        float characterG = PlayerPrefs.GetFloat("CAR_CHARACTER_GREEN");
+        float characterB = PlayerPrefs.GetFloat("CAR_CHARACTER_BLUE");
+        characterColour = new Color(characterR, characterG, characterB);
+        preview.GetComponent<Image>().material.SetColor("_Color1", characterColour);
     }
 }

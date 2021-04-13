7179c26ce5fc6fa26b703620a610e78f102c3ec4
txeptsyip <lekoro@hotmail.co.uk>
2021-04-13 19:31:06 +0100

    made it so the back to main menu button didn't borked things

--- Assets/Samuelscripts/Pause.cs
+++ Assets/Samuelscripts/Pause.cs
@@ -3,49 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon;
 
-public class Pause : MonoBehaviour
+namespace Photon.Pun.Demo.Asteroids
 {
-    bool paused;
-    public GameObject PauseMenu;
-    public GameObject OptionsMenu;
-
-    public bool Paused { get => paused; set => paused = value; }
-
-    void Update()
+    public class Pause : MonoBehaviour
     {
-        Debug.Log("pause script is running");
-        if (Input.GetKeyDown(KeyCode.Escape))
+        bool paused;
+        public GameObject PauseMenu;
+        public GameObject OptionsMenu;
+        public string LoadScene;
+
+        public bool Paused { get => paused; set => paused = value; }
+
+        void Update()
         {
-            Debug.Log("esc was pressed!");
-            SetPaused();
-        } 
-    }
-    public void SetPaused()
-    {
-        paused = !paused;
-        if (paused)
-        {
-            //Time.timeScale = 0;
-            PauseMenu.SetActive(true);
-            Debug.Log("paused");
+            Debug.Log("pause script is running");
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("esc was pressed!");
+                SetPaused();
+            }
         }
-
-        else
+        public void SetPaused()
         {
-            //Time.timeScale = 1;
+            paused = !paused;
+            if (paused)
+            {
+                //Time.timeScale = 0;
+                PauseMenu.SetActive(true);
+                Debug.Log("paused");
+            }
+
+            else
+            {
+                //Time.timeScale = 1;
+                PauseMenu.SetActive(false);
+                Debug.Log("unpaused");
+            }
+        }
+        public void OptionsButton()
+        {
+            OptionsMenu.SetActive(true);
             PauseMenu.SetActive(false);
-            Debug.Log("unpaused");
         }
-    }
-    public void OptionsButton()
-    {
-        OptionsMenu.SetActive(true);
-        PauseMenu.SetActive(false);
-    }
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("ReworkedMainMenu");
-        Debug.Log("the main menu button was pressed");
+        public void MainMenu()
+        {
+            Debug.Log("the main menu button was pressed");
+            PhotonNetwork.LeaveLobby();
+            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.Disconnect();
+            PhotonNetwork.LoadLevel(LoadScene);
+
+        }
     }
 }

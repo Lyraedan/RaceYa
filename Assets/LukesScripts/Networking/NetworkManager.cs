3fefab597fe7b459e858d918dcf79dae1bcba44f
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-04-16 11:15:42 +0100

    Removed old timer method

--- Assets/LukesScripts/Networking/NetworkManager.cs
+++ Assets/LukesScripts/Networking/NetworkManager.cs
@@ -22,10 +22,5 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         playerObject = PhotonNetwork.Instantiate(this.playerObject.name, Spawner.instance.selectedSpawn.position, Quaternion.identity, 0);
         playerObject.transform.Rotate(new Vector3(0, 90, 0));
         playerObject.GetComponent<NetworkedUser>().started = false;
-        if (PhotonNetwork.PlayerList.Length >= Spawner.instance.lobbySize) //This is the check that everyone is in
-        {
-            yield return new WaitForSeconds(4);
-            playerObject.GetComponent<NetworkedUser>().started = true;
-        }
     }
 }

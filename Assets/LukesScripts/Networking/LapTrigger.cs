2e51fa242eb577c094c961fc0ca2cecda1417841
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-04-28 22:32:10 +0100

    Lap music implemented

--- Assets/LukesScripts/Networking/LapTrigger.cs
+++ Assets/LukesScripts/Networking/LapTrigger.cs
@@ -12,6 +12,9 @@ public class LapTrigger : MonoBehaviour
     public static LapTrigger instance;
     public bool active = false;
 
+    public AudioSource audioSource;
+    public AudioClip lapOne, lapTwo, lapThree;
+
     public List<LapProgressionTrigger> progressionTriggers = new List<LapProgressionTrigger>();
 
     private void Awake()
@@ -25,10 +28,13 @@ public class LapTrigger : MonoBehaviour
     private void Start()
     {
         // Auto assign the progression id's in corrolation to the order in the list
-        for(int i = 0; i < progressionTriggers.Count; i++)
+        for (int i = 0; i < progressionTriggers.Count; i++)
         {
             progressionTriggers[i].progressionID = i;
         }
+        audioSource.clip = lapOne;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -43,7 +49,26 @@ public class LapTrigger : MonoBehaviour
                 if (!active) return;
 
                 user.NextLap();
-                if(user.currentLap <= user.maxLaps)
+                if (audioSource.isPlaying)
+                    audioSource.Stop();
+                switch (user.currentLap)
+                {
+                    case 0:
+                        audioSource.clip = lapOne;
+                        break;
+                    case 1:
+                        audioSource.clip = lapTwo;
+                        break;
+                    case 2:
+                        audioSource.clip = lapThree;
+                        break;
+                    default:
+                        audioSource.clip = lapOne;
+                        break;
+                }
+                audioSource.loop = true;
+                audioSource.Play();
+                if (user.currentLap <= user.maxLaps)
                     user.lapCounter.text = $"Lap: {user.currentLap}/{user.maxLaps}";
                 active = false;
             }

9da05259276aa998f42b7bd76a22adf74ac34acc
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-04-29 16:33:26 +0100

    Animated drinking birds

---
+++ Assets/LukesScripts/Animations/LocalAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalAnimation : MonoBehaviour
+{
+     Vector3 localPos;
+    bool wasPlaying = false;
+    public new Animation animation;
+
+    void Awake()
+    {
+        localPos = transform.position;
+        wasPlaying = false;
+        animation = GetComponent<Animation>();
+    }
+
+    void LateUpdate()
+    {
+        if (!animation.isPlaying && !wasPlaying)
+            return;
+
+        transform.localPosition += localPos;
+
+        wasPlaying = animation.isPlaying;
+    }
+}

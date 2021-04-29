0f0221ab55d969a7e4a519ca9e9f05adc2b9760d
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2021-04-29 21:22:33 +0100

    Victory music now affected by audio settings

---
+++ Assets/LukesScripts/Audio/VictoryMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryMusic : MonoBehaviour
+{
+    public AudioSource input;
+
+    private void Update()
+    {
+        input.volume = (AudioManager.instance.GetMasterVolume() * AudioManager.instance.GetMusicVolume()) / 1f;
+    }
+}
